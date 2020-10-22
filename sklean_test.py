71d1d1d1a89645ac2c0db45a51cee5ca9d09cd37
quaxoc <58987749+quaxoc@users.noreply.github.com>
2020-10-22 17:14:35 +0200

    sklearn

---
+++ sklean_test.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 22 16:58:41 2020
+
+@author: quaxoc
+"""
+
+import numpy as np
+from sklearn import preprocessing
+
+dataset=Datos('ConjuntosDatos/tic-tac-toe.data')
+data_table=dataset.datos
+le = preprocessing.LabelEncoder()
+a=np.empty([data_table.shape[0], data_table.shape[1]])
+for i in range(data_table.shape[1]):
+    column=data_table[:,i]
+    column=le.fit_transform(column)
+    a[:,i]=np.transpose(column)
+
+print(a.shape)   
+print(a) 
